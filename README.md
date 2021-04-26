de3639f614bc5a9fdaa4661569a4e608fc10fae0
zenghui <410486047@qq.com>
2021-04-26 09:20:34 +0800

    work stealing pool

--- README.md
+++ README.md
@@ -1942,16 +1942,6 @@ public class MyRejectedHandler {
 
 ## ForkJoinPool
 
-参考：
-
-- [ForkJoinPool 的使用以及原理](https://my.oschina.net/xinxingegeya/blog/3007257)
-
-- [聊聊并发（八）——Fork/Join 框架介绍](https://www.infoq.cn/article/fork-join-introduction)
-
-ForkJoinPool 使用示例：ForkJoinPoolUsage.java
-
-### RecursiveTask 和 RecursiveAction 的区别
-
 - RecursiveAction
 
 它是一种没有任何返回值的任务。只是做一些工作，比如写数据到磁盘，然后就退出了。 一个RecursiveAction可以把自己的工作分割成更小的几块， 这样它们可以由独立的线程或者CPU执行。 我们可以通过继承来实现一个RecursiveAction。
@@ -1960,7 +1950,28 @@ ForkJoinPool 使用示例：ForkJoinPoolUsage.java
 
 它是一种会返回结果的任务。可以将自己的工作分割为若干更小任务，并将这些子任务的执行合并到一个集体结果。 可以有几个水平的分割和合并。
 
-示例
+ForkJoinPool 使用示例：ForkJoinPoolUsage.java
+
+流式API底层也是ForkJoinPool实现的。
+
+MapReduce模型
+
+- 定义任务的时候，实现ForkJoinTask（原始）
+- 或者使用 RecursiveAction（不带返回值） ，RecursiveTask（带返回值）
+
+参考：
+
+- [ForkJoinPool 的使用以及原理](https://my.oschina.net/xinxingegeya/blog/3007257)
+
+- [聊聊并发（八）——Fork/Join 框架介绍](https://www.infoq.cn/article/fork-join-introduction)
+
+## WorkStealingPool
+
+> 每个线程都有单独的队列，每个线程队列执行完毕后，就会去其他的线程队列里面拿过来执行, 底层是：ForkJoinPool
+
+- 会自动启动cpu核数个线程去执行任务
+
+使用示例：WorkStealingPoolUsage.java
 
 ## 思维导图
 
---
+++ src/main/java/git/snippets/juc/WorkStealingPoolUsage.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+package git.snippets.juc;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class WorkStealingPoolUsage {
+    public static void main(String[] args) throws IOException {
+        int core = Runtime.getRuntime().availableProcessors();
+        //  会自动启动cpu核数个线程去执行任务 ,其中第一个是1s执行完毕,其余都是2s执行完毕,
+        //  有一个任务会进行等待,当第一个执行完毕后,会再次偷取最后一个任务执行
+        ExecutorService service = Executors.newWorkStealingPool();
+        service.execute(new R(1000));
+        for (int i = 0; i < core; i++) {
+            service.execute(new R(2000));
+        }
+        //由于产生的是精灵线程（守护线程、后台线程），主线程不阻塞的话，看不到输出
+        System.in.read();
+    }
+
+    static class R implements Runnable {
+
+        int time;
+
+        R(int t) {
+            this.time = t;
+        }
+
+        @Override
+        public void run() {
+            try {
+                TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(time + " " + Thread.currentThread().getName());
+        }
+    }
+}
