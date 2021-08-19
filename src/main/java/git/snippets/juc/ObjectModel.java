e878e826601c47a0d5531b25aed817bcda3e635a
zenghui <410486047@qq.com>
2021-08-19 11:28:45 +0800

    modify notes

--- src/main/java/git/snippets/juc/ObjectModel.java
+++ src/main/java/git/snippets/juc/ObjectModel.java
@@ -7,15 +7,26 @@ import org.openjdk.jol.info.ClassLayout;
  * @author <a href="mailto:410486047@qq.com">Grey</a>
  * @since
  */
+// 配置VM参数 -XX:+UseCompressedClassPointers
 public class ObjectModel {
     public static void main(String[] args) {
-        T o = new T();
+        System.out.println("======T1=======");
+        T1 o = new T1();
         String s = ClassLayout.parseInstance(o).toPrintable();
         System.out.println(s);
+        System.out.println("======T1=======");
+        System.out.println("======T2=======");
+        T2 o1 = new T2();
+        String s1 = ClassLayout.parseInstance(o1).toPrintable();
+        System.out.println(s1);
+        System.out.println("======T2=======");
     }
-    static class  T{
+    static class  T1{
         public int a = 3;
-        public long b = 3l;
+    }
+    static class T2{
+        public int a = 3;
+        public long b = 3L;
     }
 }
 
