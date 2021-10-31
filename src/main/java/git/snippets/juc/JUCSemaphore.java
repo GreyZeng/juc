74a34eeb4cb126d636a9b4d179a40ad1a278379d
GreyZeng <410486047@qq.com>
2021-10-31 23:55:50 +0800

    juc semaphore

---
+++ src/main/java/git/snippets/juc/JUCSemaphore.java
@@ -0,0 +1,67 @@
+package git.snippets.juc;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+class Log {
+    public static void println(String s) {
+        System.out.println(Thread.currentThread().getName() + ": " + s);
+    }
+}
+
+class BoundedResource {
+    private final Semaphore semaphore;
+    private final int permits;
+    private final static Random random = new Random(314159);
+
+    public BoundedResource(int permits) {
+        this.semaphore = new Semaphore(permits);
+        this.permits = permits;
+    }
+
+    public void use() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doUse();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    protected void doUse() throws InterruptedException {
+        Log.println("BEGIN: used = " + (permits - semaphore.availablePermits()));
+        Thread.sleep(random.nextInt(500));
+        Log.println("END:   used = " + (permits - semaphore.availablePermits()));
+    }
+}
+
+class UserThread extends Thread {
+    private final static Random random = new Random(26535);
+    private final BoundedResource resource;
+
+    public UserThread(BoundedResource resource) {
+        this.resource = resource;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                resource.use();
+                Thread.sleep(random.nextInt(3000));
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+public class JUCSemaphore {
+    public static void main(String[] args) {
+        // 3�̃��\�[�X��p�ӂ���
+        BoundedResource resource = new BoundedResource(3);
+
+        // 10�̃X���b�h�����p����
+        for (int i = 0; i < 10; i++) {
+            new UserThread(resource).start();
+        }
+    }
+}
