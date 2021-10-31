5b49a66a534c741ba85cda0ce16bc422ac02fd98
GreyZeng <410486047@qq.com>
2021-10-31 19:51:45 +0800

    modify code

--- src/main/java/git/snippets/juc/HelloThread.java
+++ src/main/java/git/snippets/juc/HelloThread.java
@@ -7,20 +7,21 @@ import java.util.concurrent.*;
  *
  * @author <a href="mailto:410486047@qq.com">Grey</a>
  * @date 2021/7/7
- * @since
+ * @since 1.8
  */
 public class HelloThread {
     public static void main(String[] args) throws Exception {
         MyFirstThread t1 = new MyFirstThread();
         Thread t2 = new Thread(new MySecondThread());
-        Thread t3 = new Thread(new FutureTask<>(new CallableThreadTest())); 
+        Thread t3 = new Thread(new FutureTask<>(new CallableThreadTest()));
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.execute(()-> System.out.println("方式3：使用线程池来创建线程。"));
+        executor.execute(() -> System.out.println("方式3：使用线程池来创建线程。"));
         t1.start();
         t2.start();
-        t3.start(); 
+        t3.start();
         executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.SECONDS);       
+        boolean b = executor.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println(b ? "停止成功" : "停止失败");
     }
 
     static class MyFirstThread extends Thread {
@@ -48,8 +49,8 @@ public class HelloThread {
             for (i = 0; i < 10; i++) {
                 i++;
             }
-            System.out.println("方式4，实现Callable接口方式来创建有返回值的线程，返回值是："+i);
+            System.out.println("方式4，实现Callable接口方式来创建有返回值的线程，返回值是：" + i);
             return i;
         }
-    } 
+    }
 }
