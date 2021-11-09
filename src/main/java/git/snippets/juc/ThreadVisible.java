b32acdf1248bedab02496f22b1930e9671bea9c2
zenghui <410486047@qq.com>
2021-11-09 14:43:49 +0800

    modify code

--- src/main/java/git/snippets/juc/ThreadVisible.java
+++ src/main/java/git/snippets/juc/ThreadVisible.java
@@ -7,48 +7,48 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 并发编程三大特性之:可见性
- * 
+ *
  * @author <a href="mailto:410486047@qq.com">Grey</a>
  * @since 1.8
  */
 public class ThreadVisible {
 
-  static volatile boolean flag = true;
+    static volatile boolean flag = true;
 
-  public static void main(String[] args) throws Exception {
-    Thread t = new Thread(() -> {
-      while (flag) {
-        // 如果这里调用了System.out.println()
-        // 会无论flag有没有加volatile,数据都会同步
-        // 因为System.out.println()背后调用的synchronized
-        // System.out.println();
-      }
-      System.out.println("t end");
-    });
-    t.start();
-    TimeUnit.SECONDS.sleep(3);
-    flag = false;
+    public static void main(String[] args) throws Exception {
+        Thread t = new Thread(() -> {
+            while (flag) {
+                // 如果这里调用了System.out.println()
+                // 会无论flag有没有加volatile,数据都会同步
+                // 因为System.out.println()背后调用的synchronized
+                // System.out.println();
+            }
+            System.out.println("t end");
+        });
+        t.start();
+        TimeUnit.SECONDS.sleep(3);
+        flag = false;
 
 
-    // volatile修饰引用变量
-    new Thread(a::m, "t2").start();
-    TimeUnit.SECONDS.sleep(2);
-    a.flag = false;
+        // volatile修饰引用变量
+        new Thread(a::m, "t2").start();
+        TimeUnit.SECONDS.sleep(2);
+        a.flag = false;
 
-    // 阻塞主线程,防止主线程直接执行完毕,看不到效果
-    System.in.read();
-  }
-
-  private static volatile A a = new A();
-
-  static class A {
-    /* volatile */ boolean flag = true;
-
-    void m() {
-      System.out.println("m start");
-      while (flag) {
-      }
-      System.out.println("m end");
+        // 阻塞主线程,防止主线程直接执行完毕,看不到效果
+        System.in.read();
     }
-  }
+
+    private static volatile A a = new A();
+
+    static class A {
+        /* volatile */ boolean flag = true;
+
+        void m() {
+            System.out.println("m start");
+            while (flag) {
+            }
+            System.out.println("m end");
+        }
+    }
 }
