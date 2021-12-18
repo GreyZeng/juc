0bcf1977ef79aa875c876e8b5f299792d8ec57bd
GreyZeng <410486047@qq.com>
2021-12-18 18:24:55 +0800

    post demo

--- src/main/java/git/snippets/dp2src/Balking/Sample/Main.java
+++ src/main/java/git/snippets/dp2src/Balking/Sample/Main.java
@@ -1,7 +1,12 @@
 package git.snippets.dp2src.Balking.Sample;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(() -> System.out.println("helo"));
         Data data = new Data("data.txt", "(empty)");
         new ChangerThread("ChangerThread", data).start();
         new SaverThread("SaverThread", data).start();
--- src/main/java/git/snippets/dp2src/Balking/Sample/SaverThread.java
+++ src/main/java/git/snippets/dp2src/Balking/Sample/SaverThread.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 
 public class SaverThread extends Thread {
     private final Data data;
+
     public SaverThread(String name, Data data) {
         super(name);
         this.data = data;
     }
+
     public void run() {
         try {
             while (true) {
-                data.save();            // �f�[�^��ۑ����悤�Ƃ���
-                Thread.sleep(1000);     // ��1�b�x��
+                data.save();
+                Thread.sleep(1000);
             }
         } catch (IOException e) {
             e.printStackTrace();
